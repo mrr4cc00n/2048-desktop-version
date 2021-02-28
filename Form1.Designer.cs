518f5f128ad477f2dd46e86129192069dae58eda
manueldecub@gmail.com <950628mnm>
2021-02-28 13:56:42 -0500

    feature: 2048 Desktop Game - A version of the popular android game

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Windowsentregar
+{
+    partial class planilla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(planilla));
+            this.mainmenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.size = new System.Windows.Forms.ToolStripComboBox();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fondo = new System.Windows.Forms.PictureBox();
+            this.mejorpunto = new System.Windows.Forms.Label();
+            this.puntos = new System.Windows.Forms.Label();
+            this.mainmenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainmenu
+            // 
+            this.mainmenu.BackColor = System.Drawing.Color.Transparent;
+            this.mainmenu.Font = new System.Drawing.Font("Arial Black", 9F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainmenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainmenu.Location = new System.Drawing.Point(0, 0);
+            this.mainmenu.Name = "mainmenu";
+            this.mainmenu.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.mainmenu.Size = new System.Drawing.Size(508, 25);
+            this.mainmenu.TabIndex = 0;
+            this.mainmenu.Text = "mimenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.restartToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.sizeToolStripMenuItem});
+            this.optionsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(70, 21);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.size});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.sizeToolStripMenuItem.Text = "Size";
+            // 
+            // size
+            // 
+            this.size.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16"});
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(121, 23);
+            this.size.Text = "4";
+            this.size.TextChanged += new System.EventHandler(this.size_TextChanged);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.howToPlayToolStripMenuItem});
+            this.helpToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(50, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // howToPlayToolStripMenuItem
+            // 
+            this.howToPlayToolStripMenuItem.Name = "howToPlayToolStripMenuItem";
+            this.howToPlayToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.howToPlayToolStripMenuItem.Text = "How to play";
+            this.howToPlayToolStripMenuItem.Click += new System.EventHandler(this.howToPlayToolStripMenuItem_Click);
+            // 
+            // fondo
+            // 
+            this.fondo.BackColor = System.Drawing.Color.Transparent;
+            this.fondo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fondo.Location = new System.Drawing.Point(24, 76);
+            this.fondo.Name = "fondo";
+            this.fondo.Size = new System.Drawing.Size(358, 365);
+            this.fondo.TabIndex = 1;
+            this.fondo.TabStop = false;
+            this.fondo.Paint += new System.Windows.Forms.PaintEventHandler(this.fondo_Paint);
+            // 
+            // mejorpunto
+            // 
+            this.mejorpunto.AutoSize = true;
+            this.mejorpunto.BackColor = System.Drawing.Color.Transparent;
+            this.mejorpunto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mejorpunto.Location = new System.Drawing.Point(421, 134);
+            this.mejorpunto.Name = "mejorpunto";
+            this.mejorpunto.Size = new System.Drawing.Size(65, 13);
+            this.mejorpunto.TabIndex = 3;
+            this.mejorpunto.Text = "BestScore";
+            this.mejorpunto.TextChanged += new System.EventHandler(this.mejorpunto_TextChanged);
+            // 
+            // puntos
+            // 
+            this.puntos.AutoSize = true;
+            this.puntos.BackColor = System.Drawing.Color.Transparent;
+            this.puntos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.puntos.Location = new System.Drawing.Point(432, 64);
+            this.puntos.Name = "puntos";
+            this.puntos.Size = new System.Drawing.Size(40, 13);
+            this.puntos.TabIndex = 4;
+            this.puntos.Text = "Score";
+            this.puntos.TextChanged += new System.EventHandler(this.puntos_TextChanged);
+            // 
+            // planilla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(508, 453);
+            this.Controls.Add(this.puntos);
+            this.Controls.Add(this.mejorpunto);
+            this.Controls.Add(this.fondo);
+            this.Controls.Add(this.mainmenu);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainmenu;
+            this.MaximizeBox = false;
+            this.Name = "planilla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2048";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.planilla_FormClosed);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.planilla_PreviewKeyDown);
+            this.mainmenu.ResumeLayout(false);
+            this.mainmenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fondo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainmenu;
+        private System.Windows.Forms.PictureBox fondo;
+        private System.Windows.Forms.Label mejorpunto;
+        private System.Windows.Forms.Label puntos;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToPlayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox size;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_de_pro;
+using You_Win;
+using Perdedor;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Windowsentregar
+{
+    public partial class planilla : Form
+    {
+        ClaseProyecto tab;
+        int dimensiones;
+        public planilla() 
+        {
+            InitializeComponent();
+            fondo.Focus();
+            dimensiones = int.Parse(size.Text);
+            tab = new ClaseProyecto(dimensiones);
+        }
+        #region pintando 
+        private void fondo_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush brocha1 = new SolidBrush(Color.FromArgb(10, Color.Black));
+            Graphics artista = e.Graphics;
+            Pen lapiz = new Pen(Color.Azure,2);
+            float tamaño = fondo.Width / dimensiones;
+            for (int i = 0; i < dimensiones; i++)//recorre para pintar dependiendo de la posicion
+            {
+                for (int j = 0; j < dimensiones; j++)
+                {
+                    artista.FillRectangle(brocha1, i * tamaño, j * tamaño, tamaño, tamaño);
+                    artista.DrawRectangle(lapiz, i * tamaño, j * tamaño, tamaño, tamaño);
+                    
+                     if (tab.Tablero[i, j] == 2)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\2.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 4)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\4.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 8)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\8.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 16)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\16.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 32)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\32.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 64)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\64.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 128)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\128.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 256)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\256.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 512)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\512.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 1024)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\1024.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 2048)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\2048.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 4096)
+                        artista.DrawImage(Image.FromFile(@"..\..\Imagenes\4096.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 8192)
+                         artista.DrawImage(Image.FromFile(@"..\..\Imagenes\8192.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                     if (tab.Tablero[i, j] == 16384)
+                         artista.DrawImage(Image.FromFile(@"..\..\Imagenes\16384.PNG"), j * tamaño, i * tamaño, tamaño, tamaño);
+                    
+                         if ( tab.Tablero[i, j] == 2048 && tab.count == 0)
+                         {
+                             Form Victoria = new Ganador();//muestra un form
+                             Victoria.ShowDialog();
+                             tab.count++;
+                         }
+                     
+                     
+                     
+                }
+            }
+        }
+#endregion
+        #region MOViendose
+
+        private void planilla_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            var x = e.KeyCode;
+            switch (x)
+            {
+                case Keys.Down: tab.Abajo(); fondo.Invalidate(); break;
+                case Keys.Left: tab.Izquierda(); fondo.Invalidate(); break;
+                case Keys.Right: tab.Derecha(); fondo.Invalidate(); break;
+                case Keys.Up: tab.Arriba(); fondo.Invalidate(); break;
+                case Keys.Back: undoToolStripMenuItem_Click(sender, null); break;
+                default: break;
+            }
+            puntos.Text = Convert.ToString(tab.score);
+            if (tab.BestScore < tab.score)
+            {
+                tab.BestScore = tab.score;
+                mejorpunto.Text = Convert.ToString(tab.BestScore);
+            }
+
+            if (tab.Perdiste())
+            {
+                Form Badluck = new Lose();
+                Badluck.ShowDialog();
+                tab.Iniciar(dimensiones);
+                fondo.Invalidate();
+            }
+                
+        }
+
+        #endregion
+        #region EMpezar
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tab.Iniciar(dimensiones);//inicia el tablero
+            mejorpunto.Text = Convert.ToString(tab.BestScore);
+            puntos.Text = Convert.ToString(tab.score);
+            fondo.Invalidate();
+        }
+        #endregion
+        #region REcomenzar
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tab.SaveBestScore();
+            tab.Iniciar(dimensiones);
+            fondo.Invalidate();
+        }
+       #endregion
+        #region UNdo
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tab.salvatab.Count > 0)
+            {
+                tab.Tablero = (int[,])tab.salvatab.Pop().Clone();
+                tab.score = tab.salvascore.Pop();
+                puntos.Text = Convert.ToString(tab.score);
+                fondo.Invalidate();
+            }
+            else
+                MessageBox.Show("No es posible seguir hacia atras");
+                 
+
+
+        }
+        #endregion
+        #region CArgar
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tab.CargarGame();
+                tab.LoadStacks();
+                fondo.Invalidate();
+            }
+            catch 
+            {
+                MessageBox.Show("No hay partidas guardadas intenta salvar primero");
+            }
+            
+        }
+        
+        #endregion
+        #region SAlvar
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tab.SaveStacks();
+            tab.GuardarGame();
+            fondo.Invalidate();
+        }
+#endregion
+        #region HElp
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Realizado por Manuel Inc con el apoyo especial del Team Silver y weboo");
+        }
+
+        private void howToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mueva convenientemente los controles de Arriba,Abajo,Derecha,Izquierda ");
+        }
+        #endregion
+        #region Tamaño
+        private void size_TextChanged(object sender, EventArgs e)
+        {
+            dimensiones = int.Parse(size.Text);
+        }
+        #endregion
+     
+        
+        #region bestScore
+        private void mejorpunto_TextChanged(object sender, EventArgs e)
+        {
+            mejorpunto.Text = Convert.ToString(tab.BestScore);
+        }
+        #endregion
+        #region sCORE
+        private void puntos_TextChanged(object sender, EventArgs e)
+        {
+            puntos.Text = Convert.ToString(tab.score);
+        }
+        #endregion
+        #region Cerrar
+        private void planilla_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            tab.SaveBestScore();
+        }
+        #endregion
+
+
+    }
+}
